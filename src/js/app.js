ffc1a28d1fae74b2abf043687a379c85d87bf640
Michael Sprague <mikesprague@gmail.com>
2020-07-06 13:23:26 -0400

    :recycle: Update template files for this app

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,18 @@
 import '../scss/styles.scss';
+import dayjs from 'dayjs';
 
 const initApp = () => {
-  // iniitialize app here
+  const shutdownDay = 22;
+  const shutdownDate = dayjs(`2020-03-${shutdownDay}`);
+  const today = dayjs();
+  const totalDays = today.diff(shutdownDate, 'days');
+  const todaysDate = totalDays + shutdownDay;
+  const h1el = document.querySelector('h1');
+  const h2el = document.querySelector('h2');
+  const fullDateString = `March ${todaysDate}, 2020`;
+  h1el.classList.remove('invisible');
+  h2el.textContent = fullDateString;
+  window.document.title = `${fullDateString} | ${window.document.title}`;
 };
 
 document.onreadystatechange = () => {
